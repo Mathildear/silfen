4bbf7cfc70406b483057a99ea8a37eec2ff8a89a
ReiSikk <reix0010@stud.kea.dk>
2022-04-29 14:04:50 +0200

    burgermenuJavastart

---
+++ burgermenu.js
@@ -0,0 +1,39 @@
+/* //// Burger menu
+let navBtn = document.querySelector(".burgerMenu");
+let navLinks = document.querySelector(".burgerMenuItems");
+let overlay = document.querySelector(".overlay");
+
+navBtn.addEventListener("click", burgerDrop);
+
+function burgerDrop() {
+  console.log("burgerDrop");
+  navLinks.classList.toggle("hidden");
+  overlay.classList.toggle("hidden");
+}
+if (navBtn.style.display === "block") {
+  navLinks.style.display = "block";
+  navBtn.style.display = "none";
+  console.log("optionA");
+} else if (navBtn.style.display === "none") {
+  navLinks.style.display = "none";
+  navBtn.style.display = "block";
+  console.log("optionB");
+}
+console.log("burgerClick");
+ */
+let menuIcon = document.querySelector(".menuIcon");
+let navLinks = document.querySelector(".burgeMenuItems");
+
+menuIcon.addEventListener("click", menuDrop);
+
+function menuDrop() {
+  console.log("menuDrop");
+  navLinks.classList.toggle("hidden");
+}
+if (menuIcon.style.display === "block") {
+  navLinks.style.display = "block";
+  menuIcon.style.display = "none";
+} else if (menuIcon.style.display === "none") {
+  navLinks.style.display = "none";
+  menuIcon.style.display = "block";
+}
